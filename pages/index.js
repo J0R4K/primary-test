1d2037fd98c54d531d2041462b994c18a7883e51
J0R4K <joaopedrobarruzi82@gmail.com>
2025-11-22 17:32:59 +0000

    moficia a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-     return <h1>Rayssa, y love se vc me ama tmb da uma risadinha</h1>
+     return <h1>Rayssa, Eu te amo, se vc me ama tmb da uma risadinha</h1>
 }
 
 export default Home;
