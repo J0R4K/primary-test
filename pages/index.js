31b48af0f89dd340537b0622cab52ed3a7738e08
J0R4K <joaopedrobarruzi82@gmail.com>
2025-11-22 17:42:45 +0000

    eu vou conseguir!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-     return <h1>Rayssa, Eu te amo, se vc me ama tmb da uma risadinha</h1>
+     return <h1>Rayssa, Eu te amo, se vc me ama tmb da uma risadinha. esse cara me desafio a passar da muralha da tecnologia vou ter q virar programador mesmo agr.
+
+     </h1>
 }
 
 export default Home;
