6b7fd9ad6cb55ab5ca0094cd60ceb39352730281
J0R4K <joaopedrobarruzi82@gmail.com>
2025-11-25 15:08:53 +0000

    adiciona script `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 function Home() {
-     return <h1>Rayssa, Eu te amo, se vc me ama tmb da uma risadinha. esse cara me desafio a passar da muralha da tecnologia vou ter q virar programador mesmo agr.
-
-     </h1>
+  return (
+    <h1>
+      Rayssa, Eu te amo, se vc me ama tmb da uma risadinha. esse cara me desafio
+      a passar da muralha da tecnologia vou ter q virar programador mesmo agr.
+    </h1>
+  );
 }
-
+a;
 export default Home;
